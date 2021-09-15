b42f09a5316a6a4aa607629f23a08cada7b271ee
Lisa Bost <bost.lisam@gmail.com>
2021-09-15 13:56:43 -0500

    Initial Commit Northwin Web App Contains Categories on Products Page

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class ProductController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private NorthwindContext _northwindContext;
+        public ProductController(NorthwindContext db) => _northwindContext = db;
+        public IActionResult Category() => View(_northwindContext.Categories);
+    }
+}
