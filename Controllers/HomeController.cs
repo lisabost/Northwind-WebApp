7f2a1d67d02d8293b811e8f1e6f7649ee9cd50a5
Lisa Bost <bost.lisam@gmail.com>
2021-09-28 14:11:56 -0500

    Added Toast -Toast updates with info from chosen discount -Toast plays alert sound when discount is clicked

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Models;
+using System.Linq;
 
 namespace Northwind.Controllers
 {
@@ -10,6 +11,6 @@ namespace Northwind.Controllers
         private NorthwindContext _northwindContext;
         public HomeController(NorthwindContext db) => _northwindContext = db;
 
-        public ActionResult Index() => View(_northwindContext.Discounts);
+        public ActionResult Index() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
     }
 }
--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,3 +1,30 @@
 $(function(){
-    //alert("Hello World");
+    // preload audio
+    var toastsound = new Audio('../media/toast.wav');
+
+    $('.code').on('click', function(e) {
+        e.preventDefault();
+        // first pause the audio (in case it is still playing)
+        toastsound.pause();
+        // reset the audio
+        toastsound.currentTime = 0;
+        // play audio
+        toastsound.play();
+
+        //get data-product and data-discount elements
+        var name = $(this).data("product");
+        var discount = $(this).data("code");
+
+        //update spans with product name and discount code information
+        $('#productName').text(name);
+        $('#discountCode').text(discount);
+
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
 });
+
+$(document).on('keydown', function(e) {
+    if (e.keyCode == 27) {
+        $('#toast').toast('dispose');
+    }
+})
