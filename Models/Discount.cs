07dcf7eedbf89696477926763b18ff90c8921896
Lisa Bost <bost.lisam@gmail.com>
2021-09-28 12:46:38 -0500

    Migrations -Updated migration with dotnet ef migrations add CreateDiscount

--- Models/Discount.cs
+++ Models/Discount.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Northwind.Models
 {
     public class Discount
@@ -8,6 +9,8 @@ namespace Northwind.Models
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public int ProductID { get; set; }
+
+        [Column(TypeName = "decimal(18,4)")]
         public decimal DiscountPercent { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
