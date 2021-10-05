3035492b71f717759765d92f0f482a83745d9369
Lisa Bost <bost.lisam@gmail.com>
2021-10-05 13:25:18 -0500

    AddCustomer --AddCustomer method added to northwind context class

--- Models/NorthwindContext.cs
+++ Models/NorthwindContext.cs
@@ -10,5 +10,11 @@ namespace Northwind.Models
         public DbSet<Product> Products { get; set; }
         public DbSet<Discount> Discounts { get; set; }
         public DbSet<Customer> Customer { get; set; }
+
+        public void AddCustomer(Customer customer)
+        {
+            this.Add(customer);
+            this.SaveChanges();
+        }
     }
 }
