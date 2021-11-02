7ef6aae803a157a5e4df772a1e132a7a950ea9a8
Lisa Bost <bost.lisam@gmail.com>
2021-11-02 13:55:23 -0500

    Modify the Models/UserViewModel class - Create the LoginModel class

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -8,4 +8,13 @@ namespace Northwind.Models
         [UIHint("password"), Required]
         public string Password { get; set; }
     }
+
+    public class LoginModel
+    {
+        [Required, UIHint("email")]
+        public string Email { get; set; }
+
+        [Required, UIHint("password")]
+        public string Password { get; set; }
+    }
 }
