ebe87724af11d3b35f73710c54f245942f8eb925
Lisa Bost <bost.lisam@gmail.com>
2021-11-02 13:07:13 -0500

    Modify Customer/Register controller method (http post) - Inject UserManager class into Controller using dependency injection - Add customer to Identity Db - Add customer to customer role - Add customer to Northwind Db

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,44 +1,78 @@
-using System;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Models;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Northwind.Controllers
 {
     public class CustomerController : Controller
     {
-        // this controller depends on the NorthwindRepository
+        // this controller depends on the NorthwindRepository & the UserManager
         private NorthwindContext _northwindContext;
-        public CustomerController(NorthwindContext db) => _northwindContext = db;
-        
-        //http get
+        private UserManager<AppUser> _userManager;
+        public CustomerController(NorthwindContext db, UserManager<AppUser> usrMgr)
+        {
+            _northwindContext = db;
+            _userManager = usrMgr;
+        }
         public IActionResult Register() => View();
-
-        public IActionResult CustomersList() => View(_northwindContext.Customer);
-
-        //http post
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Register(Customer model)
+        [HttpPost, ValidateAntiForgeryToken]
+        public async System.Threading.Tasks.Task<IActionResult> Register(CustomerWithPassword customerWithPassword)
         {
             if (ModelState.IsValid)
             {
-                if (_northwindContext.Customer.Any(c => c.CompanyName == model.CompanyName))
+                Customer customer = customerWithPassword.Customer;
+                if (_northwindContext.Customer.Any(c => c.CompanyName == customer.CompanyName))
                 {
-                    ModelState.AddModelError("", "Name must be unique");
+                    ModelState.AddModelError("", "Company Name must be unique");
                 }
                 else
                 {
-                    _northwindContext.AddCustomer(model);
-                    return RedirectToAction("CustomersList", "Customer");
+                    if (ModelState.IsValid)
+                    {
+                        AppUser user = new AppUser
+                        {
+                            // email and username are synced - this is by choice
+                            Email = customer.Email,
+                            UserName = customer.Email
+                        };
+                        // Add user to Identity DB
+                        IdentityResult result = await _userManager.CreateAsync(user, customerWithPassword.Password);
+                        if (!result.Succeeded)
+                        {
+                            AddErrorsFromResult(result);
+                        }
+                        else
+                        {
+                            // Assign user to customers Role
+                            result = await _userManager.AddToRoleAsync(user, "customer");
+
+                            if (!result.Succeeded)
+                            {
+                                // Delete User from Identity DB
+                                await _userManager.DeleteAsync(user);
+                                AddErrorsFromResult(result);
+                            }
+                            else
+                            {
+                                // Create customer (Northwind)
+                                _northwindContext.AddCustomer(customer);
+                                return RedirectToAction("Index", "Home");
+                            }
+                        }
+                    }
                 }
             }
             return View();
         }
 
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
     }
-
-        
-
 }
