819a82d7653e26a6cf9f6b462fb7355b0f6f2a24
BuildTools <unconfigured@null.spigotmc.org>
2021-11-09 18:19:55 -0600

    Turned off star rating

--- wwwroot/js/rating.js
+++ wwwroot/js/rating.js
@@ -1,11 +1,10 @@
 ﻿$(document).ready(function() {
     init();
     //Handle selection of rating (Caught when user clicks anywhere in the review-stars)
-    $(".review-stars").click(function(e) {
-        let targetStarIndex = $(e.target).data('index');
-        applyClass(this, 'i', 'active-star', targetStarIndex);
-    })
-    $("")
+    // $(".review-stars").click(function(e) {
+    //     let targetStarIndex = $(e.target).data('index');
+    //     applyClass(this, 'i', 'active-star', targetStarIndex);
+    // });
 });
 
 function init() {
