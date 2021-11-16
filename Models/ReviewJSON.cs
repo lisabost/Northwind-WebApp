b16a8f9df915aab6081fa3317f2aee96abf17d4b
BuildTools <unconfigured@null.spigotmc.org>
2021-11-16 15:12:29 -0600

    Worked as a team!

---
+++ Models/ReviewJSON.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Models
+{
+    public class ReviewJSON
+    {
+        public int RatingId { get; set; }
+        
+        public int Rating { get; set; }
+
+        public string Comment { get; set; }
+
+        public string Name { get; set; }
+    }
+}
