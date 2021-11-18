1afa73671436d9c665c4088db651d453b8ce75f2
BuildTools <unconfigured@null.spigotmc.org>
2021-11-18 17:57:35 -0600

    Redo-Stars

--- wwwroot/js/rating.js
+++ wwwroot/js/rating.js
@@ -1,47 +1,89 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(() => {
+
     init();
-    //Handle selection of rating (Caught when user clicks anywhere in the review-stars)
-    // $(".review-stars").click(function(e) {
-    //     let targetStarIndex = $(e.target).data('index');
-    //     applyClass(this, 'i', 'active-star', targetStarIndex);
-    // });
-    $('#reviews-header-btn').click(function() {
-        let up = $('#arrow-up');
-        let down = $('#arrow-down');
-        if(up.hasClass('d-none')) {
-            up.removeClass('d-none');
-            down.addClass('d-none');
-        } else {
-            up.addClass('d-none');
-            down.removeClass('d-none');
+
+    let mouse = { x: 0, y: 0 }
+
+    let intervalId, hoveredRatingStars, hoveredRating, savedRating;
+    $('.review-stars').hover(function() {
+        let delay = 10;
+        hoveredRatingStars = $(this);
+        if(hoveredRatingStars.data('interactive')) {
+            savedRating = hoveredRatingStars.data('rating');
+            intervalId = setInterval(interactive, delay);
+        }
+    }, function () {
+        if(hoveredRating.data('interactive')) {
+            hoveredRatingStars.data('rating', savedRating);
+            setStars(hoveredRatingStars, savedRating);
+            clearInterval(intervalId);
         }
     });
+
+    $('.review-stars').click(function () {
+        if(hoveredRatingStars.data('interactive')) savedRating = hoveredRating;
+    });
+
+    let hoverPadding = 1;
+    function interactive() {
+        hoveredRatingStars.children().each(function() {
+            if(this.matches(':hover')) {
+                // Get hovered variables
+                let $star = $(this);
+                let rect = this.getBoundingClientRect();
+                let starI = $star.data('value');
+
+                // Mouse is over star, check for full or half
+                let starWidth = rect.right - rect.left;
+                let isHalf = false;
+                if(rect.left - hoverPadding < mouse.x && mouse.x <= rect.left + starWidth/2 - hoverPadding) isHalf = true;
+
+                // Calc final value
+                let final = isHalf ? (starI - 1) : (starI);
+                hoveredRating = final;
+                setStars(hoveredRatingStars, final);
+            }
+        });
+    }
+
+    function init() {
+        $('.review-stars').each(function() {
+            let rating = $(this).data('rating');
+            setStars($(this), rating);
+        });
+    }
+
+    // Receives rating (0)1-10, converts to
+    function setStars(el, rating) {
+        if(rating === 0) return;
+        let adj = rating / 2;
+        el.children('i').each(function(index) {
+            let i = $(this);
+            if(adj > index && (adj - index) > .5) {
+                // Full Star
+                i.css('color', 'gold');
+                i.removeClass();
+                i.addClass('fas fa-star');
+            } else if((adj - index) === .5) {
+                // Half Star
+                i.css('color', 'gold');
+                i.removeClass();
+                i.addClass('fas fa-star-half-alt');
+            } else {
+                // Empty Star
+                i.css('color', 'black');
+                i.removeClass();
+                i.addClass('far fa-star');
+            }
+            // py-2 px-1 *Removed
+            i.addClass('rate-popover');
+        });
+    }
+
+    addEventListener('mousemove', (e) => {
+        mouse.x = e.clientX;
+        mouse.y = e.clientY;
+    });
+
 });
 
-function init() {
-    $('.review-stars').each(function() {
-        let avgRating = $(this).attr('data-avg');
-        applyClass(this, 'i', 'active-star', avgRating);
-    });
-}
-
-function setStars(id) {
-    let review_stars = $(`#review-${id}`);
-    let i = review_stars.data('avg');
-    applyClass(review_stars, 'i', 'active-star', i);
-}
-
-// Finds all 'el' elements in the 'parent' selector and applys the 'c' class to each up to the 'index'
-// - All 'el' elements must have a data-index attribute
-// - Subtracted 1 from index because the star data-index starts at 0 and reviews are 1 - 5;
-function applyClass(parent, el, c, index) {
-    $(parent).children(el).each(function() {
-        let adjustedIndex = index - 1;
-        let star = $(this);
-        star.removeClass(c);
-        let starI = star.attr('data-index');
-        if(starI <= adjustedIndex || starI === 0) {
-            star.addClass(c);
-        }
-    })
-}
