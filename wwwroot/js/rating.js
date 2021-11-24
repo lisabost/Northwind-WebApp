5eadad548aef9c4c831fc427ce5382d9fc1c60c8
BuildTools <unconfigured@null.spigotmc.org>
2021-11-24 03:45:50 -0600

    Toast Upgrade

--- wwwroot/js/rating.js
+++ wwwroot/js/rating.js
@@ -198,17 +198,26 @@ $('#addReviewButton').on('click', function () {
 let initToast = false;
 let index = 0;
 let toastIds = [];
+const Toast_Delay = 10000;
 function toast(header, message) {
     if(!initToast) { 
         $('body').append(`<div id="toast-component" style="position: fixed;top: 10px;right: 10px; z-index: 5;"></div>`) 
         initToast = true;
     }
     let output = `
-    <div id="toast-${index}" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div id="toast-${index}" class="shadow-lg toast" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
-            <strong id="2">${header}</strong>
+            <strong class="h5 mr-auto">${header}</strong>
+            <button type="button" class="close float-right" data-dismiss="toast" aria-label="Close">
+                &times;
+            </button>
         </div>
-        <div class="toast-body" id="toast_body">
+
+        <div class="progress" style="height: 4px;">
+            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+
+        <div class="toast-body h6" id="toast_body">
             ${message}
         </div>
     </div>
@@ -218,10 +227,9 @@ function toast(header, message) {
     toastIds.push(index);
     let timeoutId = setTimeout(() => {
         let id = `toast-${toastIds[0]}`;
-        console.log(id);
         document.getElementById(id).remove();
         toastIds.shift();
-    }, 10000);
+    }, Toast_Delay);
     index++;
 }
 
