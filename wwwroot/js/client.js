0a8d1ddb54950357b59a87901781ddecbee846e8
BuildTools <unconfigured@null.spigotmc.org>
2021-11-28 03:21:54 -0600

    Toast Upgrades

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,25 +1,27 @@
 $(function(){
     // preload audio
-    var toastsound = new Audio('../media/toast.wav');
+    // var toastsound = new Audio('../media/toast.wav');
 
     $('.code').on('click', function(e) {
+        e.stopImmediatePropagation();
         e.preventDefault();
         // first pause the audio (in case it is still playing)
-        toastsound.pause();
-        // reset the audio
-        toastsound.currentTime = 0;
-        // play audio
-        toastsound.play();
+        // toastsound.pause();
+        // // reset the audio
+        // toastsound.currentTime = 0;
+        // // play audio
+        // toastsound.play();
 
         //get data-product and data-discount elements
         var name = $(this).data("product");
         var discount = $(this).data("code");
 
         //update spans with product name and discount code information
-        console.log("Test");
-        toast(name, "Discount Code: " + discount);
+        toast(name, "Discount Code: " + discount, 'blue', true);
     });
 });
+
+
 
 $(document).on('keydown', function(e) {
     if (e.keyCode == 27) {
--- wwwroot/js/rating.js
+++ wwwroot/js/rating.js
@@ -210,7 +210,7 @@ let initToast = false;
 let index = 0;
 let toastIds = [];
 const Toast_Delay = 10000;
-function toast(header, message, color = '#1B6EC2') {
+function toast(header, message, color = '#1B6EC2', persitant=false) {
     switch (color.toUpperCase()) {
         case "RED":
             const error = new Audio('../../media/error.wav');
@@ -224,30 +224,42 @@ function toast(header, message, color = '#1B6EC2') {
         $('body').append(`<div id="toast-component" style="position: fixed;top: 10px;right: 10px; z-index: 5; width: 300px;"></div>`)
         initToast = true;
     }
+    let animationClass = "progress-bar-countdown";
+    if(persitant) {
+        animationClass = "progress-bar-persistant";
+    }
     let output = `
-    <div id="toast-${index}" class="shadow-lg toast toast-component" role="alert" aria-live="assertive" aria-atomic="true" style="border: #A9A9A9 solid 1px">
+    <div id="toast-${index}" class="shadow-lg toast" role="alert" aria-live="assertive" aria-atomic="true" style="border: #A9A9A9 solid 1px">
         <div class="toast-header">
             <strong class="h5 mr-auto" style="color: black;">${header}</strong>
             <button type="button" class="close float-right" data-dismiss="toast" aria-label="Close">
                 &times;
             </button>
         </div>
+
         <div class="progress" style="height: 4px;">
-            <div class="progress-bar" style="background-color: ${color}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            <div  class="progress-bar ${animationClass}" style="background-color: ${color}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
+
         <div class="toast-body h6" id="toast_body">
             ${message}
         </div>
     </div>
     `
     $('#toast-component').prepend(output);
-    $(`#toast-${index}`).toast({ delay: 9000 }).toast('show');
-    toastIds.push(index);
-    let timeoutId = setTimeout(() => {
-        let id = `toast-${toastIds[0]}`;
-        document.getElementById(id).remove();
-        toastIds.shift();
-    }, Toast_Delay);
+    // Post prepend changes.
+    if(persitant) {
+        $(`#toast-${index}`).toast({ autohide: false }).toast('show');
+    } else {
+        toastIds.push(index);
+        let timeoutId = setTimeout(() => {
+            let id = `toast-${toastIds[0]}`;
+            document.getElementById(id).remove();
+            toastIds.shift();
+        }, Toast_Delay);
+        $(`#toast-${index}`).toast({ delay: 9000 }).toast('show');
+    }
+    
     index++;
 }
 
@@ -295,7 +307,6 @@ $('#addToCart').on('click', function () {
         }),
         success: function (response, textStatus, jqXhr) {
             // success
-            console.log(response);
             toast("Thanks, " + response.customer.companyName + "!", response.product.productName + "was successfully added to your cart. | Cart QTY: " + response.quantity);
             resetCartModal();
         },
