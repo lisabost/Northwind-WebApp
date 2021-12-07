3f71f4af01946ba9556085a5a393460b160bdb43
Lisa Bost <bost.lisam@gmail.com>
2021-12-07 10:22:43 -0600

    Minor changes

--- Models/ReviewJSON.cs
+++ Models/ReviewJSON.cs
@@ -6,7 +6,9 @@ namespace Northwind.Models
     public class ReviewJSON
     {
         public int RatingId { get; set; }
+        [Required]
         public int Rating { get; set; }
+        [Required]
         public string Comment { get; set; }
         public string Name { get; set; }
         public int ProductId { get; set; }
